fdee2ce8e42ffe5185f8b5b4718f3a1511c7aef0
gavin0726 <lnguoxun@hotmail.com>
2021-01-12 08:53:09 +1100

    Fix the issue

--- trivia/starter/backend/test_flaskr.py
+++ trivia/starter/backend/test_flaskr.py
@@ -19,10 +19,10 @@ class TriviaTestCase(unittest.TestCase):
         setup_db(self.app, self.database_path)
 
         self.new_question = {
-            'question':'this is a test question',
-            'answer':'this is a test answer',
-            'difficulty':2,
-            'category':4
+            'question': 'this is a test question',
+            'answer': 'this is a test answer',
+            'difficulty': 2,
+            'category': 4
         }
         # binds the app to the current context
         with self.app.app_context():
@@ -30,92 +30,89 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
     """
     TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    
+    Write at least one test for each test for successful operation
+    and for expected errors.
+
     """
     def test_get_paginated_questions(self):
         res = self.client().get('/questions')
         data = json.loads(res.data)
 
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['current_category'],1)
-        self.assertEqual(data['total_questions'],18)
-        
+        self.assertEqual(data['current_category'], 1)
+        self.assertEqual(data['total_questions'], 18)
 
     def test_404_sent_requesting_beyond_valid_page(self):
         res = self.client().get('/questions?page=1000')
         data = json.loads(res.data)
 
-        self.assertEqual(res.status_code,404)
+        self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'resource not found')
 
     def test_get_question_search_with_results(self):
-        res = self.client().post('/questions',json={'searchTerm':'Africa'})
+        res = self.client().post('/questions', json={'searchTerm': 'Africa'})
         data = json.loads(res.data)
 
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(len(data['questions']),1)
+        self.assertEqual(len(data['questions']), 1)
 
     def test_get_question_search_without_results(self):
-        res = self.client().post('/questions',json={'searchTerm':'USA'})
+        res = self.client().post('/questions', json={'searchTerm': 'USA'})
         data = json.loads(res.data)
 
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(len(data['questions']),0)
+        self.assertEqual(len(data['questions']), 0)
 
     def test_create_new_question(self):
         res = self.client().post('/questions', json=self.new_question)
         data = json.loads(res.data)
-  
 
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        
-  
+
     def test_405_if_book_creation_not_allowed(self):
         res = self.client().post('/questions/45', json=self.new_question)
         data = json.loads(res.data)
-        
-        self.assertEqual(res.status_code,405)
+
+        self.assertEqual(res.status_code, 405)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'method not allowed')
-    
+
     def test_delete_question(self):
         res = self.client().delete('/questions/5')
         data = json.loads(res.data)
         question = Question.query.filter(Question.id == 5).one_or_none()
 
-
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['deletedid'],5)
+        self.assertEqual(data['deletedid'], 5)
         self.assertEqual(question, None)
 
     def test_400_if_question_does_not_exist(self):
         res = self.client().delete('/questions/1000')
         data = json.loads(res.data)
 
-        self.assertEqual(res.status_code,400)
+        self.assertEqual(res.status_code, 400)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'bad request')
-
 
     def test_given_behavior(self):
         """Test _____________ """
         res = self.client().get('/')
 
         self.assertEqual(res.status_code, 200)
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
